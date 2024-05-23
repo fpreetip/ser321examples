ec26b990f1667d18d65b378fd21a2d5454ac7fdb
fpreetip <fpreetip@asu.edu>
2024-05-22 20:38:09 -0600

    Update fraction.main

--- Gradle/JavaGradle/src/main/java/Fraction.java
+++ Gradle/JavaGradle/src/main/java/Fraction.java
@@ -43,9 +43,17 @@ public class Fraction {
          // Fraction *frac = [[Fraction alloc] init];
          Fraction frac = new Fraction();
 
-         // set the values
-         frac.setNumerator(1);
-         frac.setDenominator(3);
+         // If both numerator and denominator arguments are provided,
+         // use them; otherwise, use default values.
+         if (args.length == 2) {
+            int num = Integer.parseInt(args[0]);
+            int denom = Integer.parseInt(args[1]);
+            frac.setNumerator(num);
+            frac.setDenominator(denom);
+         } else {
+            frac.setNumerator(1);
+            frac.setDenominator(3);
+         }
 
          // print it
          System.out.print("The fraction is: ");
